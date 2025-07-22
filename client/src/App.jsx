c2745d9cc61514526519ef333189d690421cc6c7
rishabhgoshwami05 <rishabhgoshwami805@gmail.com>
2025-07-23 00:54:39 +0530

    new commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,16 @@
-import { Routes, Route } from 'react-router-dom';
-import HomePage from './components/HomePage';
-import ARViewer from './components/ARViewer';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import QRCodeSection from "./components/QRCodeSection";
+import ARViewer from "./components/ARViewer";
 
-const App = () => {
+function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/ar" element={<ARViewer />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<QRCodeSection />} />
+        <Route path="/ar" element={<ARViewer />} />
+      </Routes>
+    </BrowserRouter>
   );
-};
+}
 
 export default App;
--- client/src/components/ARViewer.jsx
+++ client/src/components/ARViewer.jsx
@@ -1,11 +1,7 @@
 const ARViewer = () => {
   return (
-    <div className="h-screen bg-black flex items-center justify-center">
-      <iframe
-        src="https://threejs.org/examples/#webgl_animation_skinning_morph"
-        title="AR Simulation"
-        className="w-full md:w-[600px] h-[400px] mb-6 border-2 border-white rounded-xl"
-      />
+    <div className="w-full h-screen flex items-center justify-center bg-black text-white text-2xl">
+      AR Content Placeholder (iframe ya Three.js yaha aayega)
     </div>
   );
 };
